d2fcf61872ab546b82cf6228051642b609741437
Jakub Wilk <jwilk@jwilk.net>
2012-02-21 14:07:35 +0100

    Import examples (2007-05-27). The ‘extensions’ directory is omitted, as the extensions have been never implemented.

---
+++ examples/good/my001.jl
@@ -0,0 +1,15 @@
+int main()
+{
+  int x, y;
+  for (x = 1; x < 5; x++)
+    for (y = 1; y < 5; y++)
+    {
+      int z = -17;
+      for (z = 1; z < 5; z++)
+        printInt(x * y * z);
+      printString("");
+    }
+  return 0;
+}
+
+/* vim:set ts=2 sw=2 et ft=c: */
---
+++ examples/good/my002.jl
@@ -0,0 +1,23 @@
+int main()
+{
+        printInt(+32 % -10);
+        printInt(-32 % -10);
+        printInt(+32 % +10);
+        printInt(-32 % +10);
+	printString("");
+        printInt(+38 % -10);
+        printInt(-38 % -10);
+        printInt(+38 % +10);
+        printInt(-38 % +10);
+	printString("");
+        printDouble(+32.0 % -10.0);
+        printDouble(-32.0 % -10.0);
+        printDouble(+32.0 % +10.0);
+        printDouble(-32.0 % +10.0);
+	printString("");
+        printDouble(+38.0 % -10.0);
+        printDouble(-38.0 % -10.0);
+        printDouble(+38.0 % +10.0);
+        printDouble(-38.0 % +10.0);
+        return 0;
+}
