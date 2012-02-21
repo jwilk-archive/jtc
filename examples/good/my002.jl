3d8f498930818a29e18d45592f85a451bb2ace61
Jakub Wilk <jwilk@jwilk.net>
2012-02-21 15:42:28 +0100

    examples: fix indentation.

--- examples/good/my002.jl
+++ examples/good/my002.jl
@@ -1,23 +1,25 @@
 int main()
 {
-        printInt(+32 % -10);
-        printInt(-32 % -10);
-        printInt(+32 % +10);
-        printInt(-32 % +10);
-	printString("");
-        printInt(+38 % -10);
-        printInt(-38 % -10);
-        printInt(+38 % +10);
-        printInt(-38 % +10);
-	printString("");
-        printDouble(+32.0 % -10.0);
-        printDouble(-32.0 % -10.0);
-        printDouble(+32.0 % +10.0);
-        printDouble(-32.0 % +10.0);
-	printString("");
-        printDouble(+38.0 % -10.0);
-        printDouble(-38.0 % -10.0);
-        printDouble(+38.0 % +10.0);
-        printDouble(-38.0 % +10.0);
-        return 0;
+  printInt(+32 % -10);
+  printInt(-32 % -10);
+  printInt(+32 % +10);
+  printInt(-32 % +10);
+  printString("");
+  printInt(+38 % -10);
+  printInt(-38 % -10);
+  printInt(+38 % +10);
+  printInt(-38 % +10);
+  printString("");
+  printDouble(+32.0 % -10.0);
+  printDouble(-32.0 % -10.0);
+  printDouble(+32.0 % +10.0);
+  printDouble(-32.0 % +10.0);
+  printString("");
+  printDouble(+38.0 % -10.0);
+  printDouble(-38.0 % -10.0);
+  printDouble(+38.0 % +10.0);
+  printDouble(-38.0 % +10.0);
+  return 0;
 }
+
+/* vim:set ts=2 sw=2 et ft=c: */
